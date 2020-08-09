f615c5c4be114dda589513f1a9a066fe9c787d24
Michael Cui <cuimichael500@gmail.com>
2020-08-09 16:25:31 -0500

    added navbar and started about section

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function stickyNav() {
     navbar.classList.remove("sticky");
   }
 }
+
+
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("readBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
