5e84e3b82afbbe52ad39b3eebd1b202b089d4ac5
Michael Cui <cuimichael500@gmail.com>
2020-08-16 01:37:20 -0500

    prettier code formatting

--- README.md
+++ README.md
@@ -2,13 +2,11 @@
 
 ## Deadlines
 
-* 1st Draft for review by August 15th
-* Ready to be connected to Liquid August 22nd
-* Ready for full testing August 28th
-* Implemented into the first campaign on September 7th
-
+- 1st Draft for review by August 15th
+- Ready to be connected to Liquid August 22nd
+- Ready for full testing August 28th
+- Implemented into the first campaign on September 7th
 
 ## Rough Draft
 
 All sections are completed and look great in both Iphone 11xPro (375 px width) and Macbook Pro (1280px width)
-
--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-
-window.onscroll = function() {
+// on scroll calls the two functions
+window.onscroll = function () {
   stickyNav();
   scrollFunction();
-}
+};
 
+// getting dom variables
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 var backtoTop = document.getElementById("backtoTop");
 windowWidth = $(window).width();
 
+// function for sticky navbar
 function stickyNav() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-    if(windowWidth > 600){
+    navbar.classList.add("sticky");
+    if (windowWidth > 600) {
       $("#navbar").width("100%");
-      $("#navbar").css("border-radius","0px");
+      $("#navbar").css("border-radius", "0px");
     }
-    
-  
   } else {
     navbar.classList.remove("sticky");
-    if (windowWidth > 600){
+    if (windowWidth > 600) {
       $("#navbar").width("90%");
-      $("#navbar").css("border-radius","30px");
+      $("#navbar").css("border-radius", "30px");
     }
-    
   }
 }
 
+// function for back to top going to top of page
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
 
-
+// function for back to top sticking
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     backtoTop.style.display = "block";
@@ -42,9 +42,9 @@ function scrollFunction() {
   }
 }
 
-
+//function for Read More Button
 function readMore() {
-  var fade = document.getElementById("bottom_fade")
+  var fade = document.getElementById("bottom_fade");
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("readBtn");
@@ -55,7 +55,6 @@ function readMore() {
     moreText.display = "none";
     fade.style.display = "block";
   } else {
-   
     fade.style.display = "none";
     dots.style.display = "none";
     btnText.style.display = "none";
